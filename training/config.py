27461fe9dc3ce554a08dfab9b25aeb24a61ddcca
Dr. Niwech Harnkham <35854141+drniwech@users.noreply.github.com>
2026-03-25 17:22:19 -0500

    Add configuration settings for healthcare ML project

---
+++ training/config.py
@@ -0,0 +1,10 @@
+PROJECT_ID = "healthcare-mlops-platform"
+DATASET = "healthcare_ml"
+FEATURE_TABLE = "features_v2"
+
+TARGET_COLUMN = "readmitted"
+
+MODEL_PATH = "training/model/model.joblib"
+METRICS_PATH = "training/model/metrics.json"
+
+MLFLOW_EXPERIMENT = "healthcare-mlops"  
