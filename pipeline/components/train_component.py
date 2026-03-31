f0ef500bb0aa66713e67d1d6c4ca781feda1731f
Dr. Niwech Harnkham <35854141+drniwech@users.noreply.github.com>
2026-03-31 15:05:20 -0500

    Refactor training component to use input/output parameters

--- pipeline/components/train_component.py
+++ pipeline/components/train_component.py
@@ -1,10 +1,25 @@
-from kfp.v2.dsl import component
+from kfp.v2.dsl import component, Input, Output, Model, Dataset
 
-@component(
-    base_image="us-central1-docker.pkg.dev/healthcare-mlops-platform/mlops-repo/healthcare-mlops:latest"
-)
-def train_model_op():
-    import subprocess
+@component
+def train_op(
+    input_data: Input[Dataset],
+    model_output: Output[Model],
+):
+    import pandas as pd
+    from sklearn.model_selection import train_test_split
+    from xgboost import XGBClassifier
+    import joblib
 
-    # Run your existing training script inside a container
-    subprocess.run(["python", "training/train.py"], check=True)
+    df = pd.read_csv(input_data.path)
+
+    X = df.drop(columns=["readmitted"])
+    y = df["readmitted"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    model = XGBClassifier()
+    model.fit(X_train, y_train)
+
+    joblib.dump(model, model_output.path)
